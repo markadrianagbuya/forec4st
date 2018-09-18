11bc402a25f3b867e5c4c8b66125b1bb1e3bf94f
Mark Agbuya <markadrianagbuya@gmail.com>
2018-09-19 03:08:19 +1000

    ref: simplify mapping and re-mapping by utilising active record model

--- app/controllers/city_forecasts_controller.rb
+++ app/controllers/city_forecasts_controller.rb
@@ -1,8 +1,6 @@
 require 'yahoo_weather'
 require 'temperature'
 
-DailyForecast = Struct.new(:date, :high, :low, :description)
-
 class CityForecastsController < ApplicationController
   def show
     @city = params[:city]
@@ -15,22 +13,13 @@ class CityForecastsController < ApplicationController
 
     daily_forecast_json_objects = response["query"]["results"]["channel"]["item"]["forecast"]
     @daily_forecasts = daily_forecast_json_objects.first(3).map do |forecast| 
-      DailyForecast.new(
-        forecast["date"],
-        Temperature.fahrenheit_to_celsius(forecast["high"]).round(1),
-        Temperature.fahrenheit_to_celsius(forecast["low"]).round(1),
-        forecast["text"]
-      )
-    end
-
-    @daily_forecasts.each do |daily_forecast|
       DayForecast.create!(
         forecasted_at: Time.parse(response["query"]["created"]),
-        forecast_date: daily_forecast.date,
-        high: daily_forecast.high,
-        low: daily_forecast.low,
+        forecast_date: forecast["date"],
+        high: Temperature.fahrenheit_to_celsius(forecast["high"]).round(1),
+        low: Temperature.fahrenheit_to_celsius(forecast["low"]).round(1),
         city: @city,
-        description: daily_forecast.description
+        description: forecast["text"]
       )
     end
   end
