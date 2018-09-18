a33caced367d87261c011f0ac12ac00403443db0
Mark Agbuya <markadrianagbuya@gmail.com>
2018-09-18 23:16:48 +1000

    feat: show 404 if city not sydney or melbourne

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'weather#index'
-  get 'city_forecast/:city', to: 'city_forecasts#show', as: 'city_forecast'
+  get 'city_forecast/:city', to: 'city_forecasts#show', as: "city_forecast", constraints: { city: /(sydney|melbourne)/ }
 end
--- spec/requests/city_forecasts_spec.rb
+++ spec/requests/city_forecasts_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "CityForecasts", type: :request do
-  describe "GET /city_forecasts/sydney" do
-    it "shows the forecasts for sydney" do
+  describe "GET /city_forecasts/:city" do
+    it "shows the forecasts for sydney when city is sydney" do
       stub_request(:get, "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D1105779&format=json").
       to_return(headers: { "content-type" => "application/json" }, status: 200, body: File.read(File.join(Rails.root, "spec/fixtures/sydney_weather_response.json")))
 
@@ -11,10 +11,8 @@ RSpec.describe "CityForecasts", type: :request do
       expect(response).to have_http_status(200)
       expect(response.body).to include("26.7°C")
     end
-  end
 
-  describe "GET /city_forecasts/melbourne" do
-    it "shows the forecasts for melbourne" do
+    it "shows the forecasts for melbourne when city is melbourne" do
       stub_request(:get, "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D1103816&format=json").
       to_return(headers: { "content-type" => "application/json" }, status: 200, body: File.read(File.join(Rails.root, "spec/fixtures/melbourne_weather_response.json")))
 
@@ -22,6 +20,12 @@ RSpec.describe "CityForecasts", type: :request do
 
       expect(response).to have_http_status(200)
       expect(response.body).to include("18.3°C")
+    end
+
+    it "returns a 404 response if city is unexpected" do
+      expect do
+        get "/city_forecast/adelaide"
+      end.to raise_error(ActionController::RoutingError)
     end
   end
 end
