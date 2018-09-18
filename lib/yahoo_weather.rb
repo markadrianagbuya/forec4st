80184e786d42211de20c6dcbe44d7e495b8816f3
Mark Agbuya <markadrianagbuya@gmail.com>
2018-09-19 01:29:20 +1000

    ref: make query params and woeid distinct in YahooWeather

--- lib/yahoo_weather.rb
+++ lib/yahoo_weather.rb
@@ -1,11 +1,37 @@
 require 'httparty'
+require 'uri'
 
 class YahooWeather
+  include HTTParty
+  base_uri 'https://query.yahooapis.com'
+
   def weather_for_sydney
-    HTTParty.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D1105779&format=json').parsed_response
+    city_forcast("sydney")
   end
 
   def weather_for_melbourne
-    HTTParty.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D1103816&format=json').parsed_response
+    city_forcast("melbourne")
+  end
+
+  private
+
+  def city_forcast(city)
+    self.class.get('/v1/public/yql', city_forecast_options(city)).parsed_response
+  end
+
+  def city_forecast_options(city)
+    {
+      query: {
+        q: "select * from weather.forecast where woeid=#{woeid_for_city(city)}",
+        format: "json"
+      }
+    }
+  end
+
+  def woeid_for_city(city)
+    {
+      "sydney" => 1105779,
+      "melbourne" => 1103816
+    }[city]
   end
 end
--- spec/lib/yahoo_weather_spec.rb
+++ spec/lib/yahoo_weather_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe YahooWeather do
   describe "#weather_for_sydney" do
     it "returns the json for the weather in sydney" do
       sydney_json_response = File.read("spec/fixtures/sydney_weather_response.json")
-      stub_request(:get, "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D1105779&format=json").
+      stub_request(:get, "https://query.yahooapis.com/v1/public/yql").with(query: { "q" => "select * from weather.forecast where woeid=1105779", "format" => :json }).
       to_return(headers: { "content-type" => "application/json" }, status: 200, body: sydney_json_response)
 
       expect(YahooWeather.new.weather_for_sydney).to eq JSON.parse(sydney_json_response)
@@ -15,7 +15,7 @@ RSpec.describe YahooWeather do
   describe "#weather_for_melbourne" do
     it "returns the json for the weather in melbourne" do
       melbourne_json_response = File.read("spec/fixtures/melbourne_weather_response.json")
-      stub_request(:get, "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D1103816&format=json").
+      stub_request(:get, "https://query.yahooapis.com/v1/public/yql").with(query: { "q" => "select * from weather.forecast where woeid=1103816", "format" => :json }).
       to_return(headers: { "content-type" => "application/json" }, status: 200, body: melbourne_json_response)
 
       expect(YahooWeather.new.weather_for_melbourne).to eq JSON.parse(melbourne_json_response)
