58c67148d599a934b2b462cc7cf978edefce1504
Mark Agbuya <markadrianagbuya@gmail.com>
2018-09-18 23:07:52 +1000

    feat: show forecast for melbourne

--- app/controllers/city_forecasts_controller.rb
+++ app/controllers/city_forecasts_controller.rb
@@ -1,6 +1,12 @@
 class CityForecastsController < ApplicationController
   def show
-    response = HTTParty.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D1105779&format=json')
+    case params[:city].downcase
+    when "sydney"
+      response = HTTParty.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D1105779&format=json')
+    when "melbourne"
+      response = HTTParty.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D1103816&format=json')
+    end
+
     temperature_in_fahrenheit = response["query"]["results"]["channel"]["item"]["forecast"].first["high"].to_d
     @temperature = (temperature_in_fahrenheit - 32.to_d) / 1.8.to_d
   end
--- spec/requests/city_forecasts_spec.rb
+++ spec/requests/city_forecasts_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe "CityForecasts", type: :request do
       expect(response.body).to include("26.7°C")
     end
   end
+
+  describe "GET /city_forecasts/melbourne" do
+    it "shows the forecasts for melbourne" do
+      stub_request(:get, "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D1103816&format=json").
+      to_return(headers: { "content-type" => "application/json" }, status: 200, body: File.read(File.join(Rails.root, "spec/fixtures/melbourne_weather_response.json")))
+
+      get city_forecast_path("melbourne")
+
+      expect(response).to have_http_status(200)
+      expect(response.body).to include("18.3°C")
+    end
+  end
 end
